38c5334befb825f648d67b963f5568217442e0e8
Nicolas <nicolas.defoort@isen.yncrea.fr>
2021-05-25 11:49:24 +0200

    Create PDF with user input

---
+++ EditionFactureAndroid/app/src/main/java/com/example/editionfactureandroid/FormChaudiere.java
@@ -0,0 +1,106 @@
+package com.example.editionfactureandroid;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class FormChaudiere extends AppCompatActivity implements View.OnClickListener {
+
+    EditText nameText,firstNameText,autreText,dateText,phoneText,prixText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Design_Light_NoActionBar);
+        setContentView(R.layout.activity_chaudiere_form);
+
+
+        Button CreatePDF = findViewById(R.id.buttonCreatePDF);
+        CreatePDF.setOnClickListener(this);
+
+
+        nameText = findViewById(R.id.editTextName);
+        firstNameText = findViewById(R.id.editTextFirstName);
+        autreText = findViewById(R.id.editTextAutre);
+        dateText = findViewById(R.id.editTextDate);
+        phoneText = findViewById(R.id.editTextPhone);
+        prixText = findViewById(R.id.editTextPrix);
+
+        TextView nameTitle = findViewById(R.id.textViewName);
+        TextView firstNameTitle = findViewById(R.id.textViewFirstName);
+        TextView autreTitle = findViewById(R.id.textViewAutre);
+        TextView phoneTitle = findViewById(R.id.textViewPhone);
+        TextView prixTitle = findViewById(R.id.textViewPrix);
+        TextView dateTitle = findViewById(R.id.textViewDate);
+
+    }
+
+
+
+    private void createPdf() throws FileNotFoundException {
+
+
+        String name = nameText.getText().toString();
+        String firstName = firstNameText.getText().toString();
+        String date = dateText.getText().toString();
+        String phone = phoneText.getText().toString();
+        String autre = autreText.getText().toString();
+        String prix = prixText.getText().toString();
+
+        String pdPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        File file = new File(pdPath,"myPDF2.pdf");
+        OutputStream outputStream= new FileOutputStream(file);
+
+        PdfWriter writer = new PdfWriter(file);
+        PdfDocument pdfDocument = new PdfDocument(writer);
+        Document document = new Document(pdfDocument);
+
+        float columnWidth[]={80,80,80,80,80,80};
+        Table table = new Table(columnWidth);
+
+        table.addCell(new Cell().add(new Paragraph(name)));
+        table.addCell(new Cell().add(new Paragraph(firstName)));
+        table.addCell(new Cell().add(new Paragraph(date)));
+        table.addCell(new Cell().add(new Paragraph(phone)));
+        table.addCell(new Cell().add(new Paragraph(autre)));
+
+        table.addCell(new Cell().add(new Paragraph(prix)));
+        table.addCell(new Cell().add(new Paragraph("gdrw")));
+        table.addCell(new Cell().add(new Paragraph("rgd")));
+        table.addCell(new Cell().add(new Paragraph("vdrd")));
+        table.addCell(new Cell().add(new Paragraph("vdr")));
+
+        document.add(table);
+        document.close();
+        Toast.makeText(this,"Pdf created", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        try {
+            createPdf();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- EditionFactureAndroid/app/src/main/java/com/example/editionfactureandroid/MainActivity.java
+++ EditionFactureAndroid/app/src/main/java/com/example/editionfactureandroid/MainActivity.java
@@ -1,14 +1,30 @@
+
 package com.example.editionfactureandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Design_Light_NoActionBar);
         setContentView(R.layout.activity_main);
+        Button formChaudiere = findViewById(R.id.buttonFactureChaudiere);
+        formChaudiere.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.buttonFactureChaudiere)
+        {
+            startActivity(new Intent(this,FormChaudiere.class));
+        }
     }
 }
+
